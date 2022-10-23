1cf52496535782b1c7d6a224a8349afe66ea701d
Simon Chung <84983335+problsolv@users.noreply.github.com>
2022-10-23 12:55:45 -0400

    Update LargePower.java

--- LargePower.java
+++ LargePower.java
@@ -1,14 +1,6 @@
 import java.util.Scanner;
 public class LargePower {
     public static void main(String [ ] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a base value: ");
-        int base = in.nextInt();
-        System.out.print("Enter an exponent value: ");
-        int exponent = in.nextInt();
-        System.out.println(valid((int)Math.pow(base, exponent)));
-    }
-    public static boolean valid(int pow) {
-        return pow > 5000;
+        System.out.println(Math.pow(new Scanner(System.in).nextInt(), new Scanner(System.in).nextInt()) > 5000);
     }
 }
